16c0cb25a7cbac8c4bf592f503dae2c422a0bc2a
Andreas Lydakis <Andreas.Lydakis@dyson.com>
2020-09-24 10:53:49 +0100

    changes to test PR rebuilds

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There, General Kenobi. .
+          Feature Branch .
         </p>
         <a
           className="App-link"
